b9971b4e9fc0fdf3ce6f0e44538cbbc849d31293
Lokesh Patel <patellokesh05899@gmail.com>
2018-10-18 19:25:47 +0530

    Implemented Comparable interface

--- Position.java
+++ Position.java
@@ -1,4 +1,4 @@
-public class Position {
+public class Position implements Comparable<Position> {
 	private PageEntry _p;
 	private int _wordIndex;
 	
@@ -17,5 +17,15 @@ public class Position {
 
 	public boolean equals(Position other) {
 		return _p.equals(other._p) && _wordIndex == other._wordIndex;
+	}
+
+	public int compareTo(Position other) {
+		if (_wordIndex < other._wordIndex) {
+			return -1;
+		} else if (_wordIndex > other._wordIndex) {
+			return 1;
+		} else {
+			return _p.getPageName().compareTo(other._p.getPageName());
+		}
 	}
 }
